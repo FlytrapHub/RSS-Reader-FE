8a8a0bbb2c902ca10f42b09af0197c300273d1e6
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-08 13:49:06 +0900

    SideBar 추가

--- src/component/layout/sidebar/SideFolderTree.tsx
+++ src/component/layout/sidebar/SideFolderTree.tsx
@@ -1,43 +1,41 @@
 import { Icon } from "../../common/Icon";
+import { Folder } from "./SideBarType";
 
 type Props = {
   title?: string,
+  folders: Folder[],
 }
 
-export default function SideFolderTree({title}: Props) {
+export default function SideFolderTree({title, folders}: Props) {
+
+  //const navigate = useNavigate();
+
+  const goToBlog = (blogId: number) => {
+    // TODO: 블로그 페이지로 이동
+    //navigate();
+    alert('blog: ' + blogId);
+  }
+
   return (
     <>
       <div className="text-lg font-bold">{title || '이름 없음'}</div>
       <ul className="menu w-70 rounded-box">
         <li>
-          <details open>
-            <summary>
-              <Icon name="folder" />
-              기술 블로그
-            </summary>
-            <ul>
-              <li>
-                <a>지니 블로그</a>
-              </li>
-              <li>
-                <a>이린 블로그</a>
-              </li>
-            </ul>
-          </details>
-          <details>
-            <summary>
-              <Icon name="folder" />
-              요리 블로그
-            </summary>
-            <ul>
-              <li>
-                <a>감자의 요리교실</a>
-              </li>
-              <li>
-                <a>듀이의 해피쿠킹</a>
-              </li>
-            </ul>
-          </details>
+          {folders && folders.length > 0 ? (folders.map((folder) =>  (
+            <details>
+              <summary>
+                <Icon name="folder" />
+                {folder.name}
+              </summary>
+              <ul>
+                {folder.blogs.map(blog => (
+                  <li>
+                    <a onClick={() => goToBlog(blog.id)}>{blog.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </details>
+          ))) : (<p>폴더가 없습니다.</p>)}
         </li>
       </ul>
     </>
--- src/component/layout/sidebar/SideMenu.tsx
+++ src/component/layout/sidebar/SideMenu.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
 import SideFolderTree from "./SideFolderTree";
+import axios from "axios";
+import { Folder } from "./SideBarType";
 
 export default function SideMenu() {
+
+  const[privateFolders, setPrivateFolders] = useState<Folder[]>([]);
+  const[sharedFolders, setSharedFolders] = useState<Folder[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_BASE_URL + "/folders", {
+        withCredentials: true,
+      })
+      .then(function (response) {
+        if (response.status == 200) {
+          const folders = response.data.data.folders;
+          setPrivateFolders(folders.PRIVATE);
+          setSharedFolders(folders.SHARED);
+        } else {
+          throw new Error("Request failed: " + response.status);
+        }
+      })
+      .catch(function (error) {
+        console.log('error: {}', error)
+      });
+  }, []);
+
   return (
     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content gap-2">
       <li>
@@ -14,10 +40,10 @@ export default function SideMenu() {
       </li>
       <div className="border-2 border-success bg-green-50 rounded-box gap-2">
         <li>
-          <SideFolderTree title={'내 폴더'} />
+          <SideFolderTree title={"내 폴더"} folders={privateFolders} />
         </li>
         <li>
-          <SideFolderTree title={'공유 폴더'} />
+          <SideFolderTree title={"공유 폴더"} folders={sharedFolders} />
         </li>
       </div>
     </ul>
