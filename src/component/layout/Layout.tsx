d8f0574d3fdb56266c5892a8b69d936bfa519d74
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-08 13:50:57 +0900

    feat: Layout 추가

--- src/component/layout/Layout.tsx
+++ src/component/layout/Layout.tsx
@@ -1,7 +1,12 @@
+import { ReactNode } from "react";
 import Header from "./header/Header";
 import SideBar from "./sidebar/SideBar";
 
-export default function Layout() {
+type Props = {
+  children: ReactNode;
+};
+
+export default function Layout({children}: Props) {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
@@ -9,6 +14,7 @@ export default function Layout() {
       <div className="drawer-content flex flex-col items-center justify-start">
         <Header />
         {/* Content */}
+        {children}
       </div>
 
       <div className="drawer-side">
