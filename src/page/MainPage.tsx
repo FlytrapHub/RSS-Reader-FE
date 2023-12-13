38fcdb17cbe5338376610d7bc476ff014cd3ea48
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-13 16:40:14 +0900

    reactor: Layout 구조 개선
     SideBar 메뉴 클릭시 화면 전체가 새로고침 되는 문제 해결

---
+++ src/page/MainPage.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+import Layout from "../component/layout/Layout";
+import { useLocation } from "react-router-dom";
+import AllPostListContent from "../component/content/AllPostListContent";
+import BookmarkListContent from "../component/content/BookmarkListContent";
+import SubscribePostListContent from "../component/content/SubscribePostListContent";
+import { Pages } from "../constants/Pages";
+
+type Props = {
+  page: Pages;
+};
+
+export default function MainPage({page} :Props) {
+  
+  const location = useLocation();
+  let headerTitle = page;
+  let content: ReactNode;
+  let key: number = 0;
+
+  switch (page) {
+    case Pages.ALL_POST: {
+      content = <AllPostListContent />
+      break;
+    }
+    case Pages.BOOKMARK: {
+      content = <BookmarkListContent />
+      break;
+    }
+    case Pages.SUBSCRIBE: {
+        const data = location.state;
+        headerTitle = data.subscribeTitle;
+        key = data.subscribeId;
+        content = <SubscribePostListContent key={key} subscribeId={data.subscribeId} />
+    }
+  }
+  
+  return (
+    <>
+      <Layout headerTitle={headerTitle}>
+          {content}
+      </Layout>
+    </>
+  );
+}
