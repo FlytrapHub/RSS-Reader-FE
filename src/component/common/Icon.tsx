118f15ba5546fefa3121f9069da705cc7e4bf3d1
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-18 16:48:00 +0900

    feat: 북마크 추가/제거 추가

--- src/component/common/Icon.tsx
+++ src/component/common/Icon.tsx
@@ -1,12 +1,16 @@
 import GitHubMark from "../../assets/github-mark-white.svg?react";
 import Folder from "../../assets/folder.svg?react";
 import Stack from "../../assets/stack.svg?react";
+import Bookmark from "../../assets/bookmark.svg?react";
+import BookmarkFill from "../../assets/bookmark-fill.svg?react";
 import React from 'react';
 
 export const icons = {
   github_mark: GitHubMark,
   folder: Folder,
   stack: Stack,
+  bookmark: Bookmark,
+  bookmarkFill: BookmarkFill,
 };
 
 type IconProps = {
