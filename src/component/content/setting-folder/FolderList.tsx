317efa412ffc073d3c6714ff902e66fc4d7c8613
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-26 21:08:20 +0900

    feat: 폴더 설정 Modal 뼈대 추가

--- src/component/content/setting-folder/FolderList.tsx
+++ src/component/content/setting-folder/FolderList.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { Folder } from "../../layout/sidebar/SideBarType";
 import FolderBox from "./FolderBox";
+import FolderModal from "./FolderModal";
 
 type Props = {
   title: string,
@@ -7,14 +9,18 @@ type Props = {
 }
 
 export default function FolderList({ title, folders }: Props) {
+
+  const [isFolderModalOpen, setIsFolderModalOpen] = useState<boolean>(false);
+
   return (
     <>
       <h1 className="text-left text-lg font-bold mt-4 px-2">{title}</h1>
       <div className="border-2 border-success bg-green-50 rounded-box gap-2">
         {folders && folders.map((folder: Folder, index: number) => (
-          <FolderBox key={index} folder={folder} />
+          <FolderBox key={index} folder={folder} setIsFolderModalOpen={setIsFolderModalOpen} />
         ))}
       </div>
+      <FolderModal isFolderModalOpen={isFolderModalOpen} setIsFolderModalOpen={setIsFolderModalOpen} />
     </>
   );
 }
