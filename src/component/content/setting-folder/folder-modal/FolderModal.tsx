38907247cf35b5522d83ba86b57da804d7c7bb40
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-31 05:33:02 +0900

    feat: FolderModal에 블로그 목록 추가

---
+++ src/component/content/setting-folder/folder-modal/FolderModal.tsx
@@ -0,0 +1,47 @@
+import Modal from "react-modal";
+import SubscribeSection from "./SubscribeSection";
+import MemberSection from "./MemberSection";
+import { Folder } from "../../../layout/sidebar/SideBarType";
+
+type Props = {
+  isFolderModalOpen: boolean,
+  setIsFolderModalOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  folder?: Folder,
+  setFolder: React.Dispatch<React.SetStateAction<Folder | undefined>>,
+};
+
+export default function FolderModal({
+  isFolderModalOpen,
+  setIsFolderModalOpen,
+  folder,
+  setFolder,
+}: Props) {
+    
+  const closeFolderModal = () => {
+    setIsFolderModalOpen(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        isOpen={isFolderModalOpen}
+        className="bg-transparent flex justify-center items-center h-full"
+      >
+        <div className="card w-9/12 h-5/6 bg-base-100 shadow-xl">
+          <button
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            onClick={closeFolderModal}
+          >
+            ✕
+          </button>
+          <h3 className="font-bold text-lg">폴더 명</h3>
+          
+          <div className="flex md:flex-row flex-col gap-2">
+            <SubscribeSection folder={folder} setFolder={setFolder} />
+            <MemberSection folder={folder} setFolder={setFolder} />
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
