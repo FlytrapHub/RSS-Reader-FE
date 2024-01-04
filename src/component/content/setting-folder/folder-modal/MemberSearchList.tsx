d33dc82fbfe51425217df9065c4f56ea8f91c846
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-01-04 23:22:51 +0900

    feat: 검색된 회원 목록 Dropbox를 absolute로 설정

--- src/component/content/setting-folder/folder-modal/MemberSearchList.tsx
+++ src/component/content/setting-folder/folder-modal/MemberSearchList.tsx
@@ -3,17 +3,20 @@ import MemberSearchBox from "./MemberSearchBox";
 
 type Props = {
   members: InvitedMember[];
-  addMember: (inviteeId: number) => void
+  addMember: (inviteeId: number) => void;
 };
 export default function MemberSearchList({ members, addMember }: Props) {
   return (
-    <ul
-      tabIndex={0}
-      className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full"
-    >
-      {members && members.map((member) => (
-        <MemberSearchBox member={member} addMember={addMember} />
-      ))}
-    </ul>
+    <div className="relative">
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full absolute"
+      >
+        {members &&
+          members.map((member) => (
+            <MemberSearchBox member={member} addMember={addMember} />
+          ))}
+      </ul>
+    </div>
   );
 }
