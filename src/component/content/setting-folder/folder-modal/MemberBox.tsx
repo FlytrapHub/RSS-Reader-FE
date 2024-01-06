e9c35311a33ce492376bc1a2f2533b817f6e38ee
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-01-06 17:52:55 +0900

    feat: 폴더에 초대된 회원 목록에 프로필 이미지 추가

--- src/component/content/setting-folder/folder-modal/MemberBox.tsx
+++ src/component/content/setting-folder/folder-modal/MemberBox.tsx
@@ -1,21 +1,23 @@
-import { Icon } from "../../../common/Icon";
 import { InvitedMember } from "../../../layout/sidebar/SideBarType";
 
 type Props = {
-  member: InvitedMember,
-}
+  member: InvitedMember;
+};
 
 export default function MemberBox({ member }: Props) {
-    return (
-        <div className="h-12 flex items-center hover:bg-success rounded-lg">
-          <div className="flex flex-none justify-center w-1/12 mx-3">
-            <Icon name="folder" size="L" />
+  return (
+    <div className="h-12 flex items-center hover:bg-success rounded-lg">
+      <div className="flex flex-none justify-center w-1/12 mx-3">
+        <div className="avatar">
+          <div className="w-8 rounded-full">
+            <img src={member.profile} />
           </div>
-          <div className="flex-1 w-full">
-            <div className="text-lg text-left">{member.name}</div>
-            <div className="text-sm text-left text-gray-400">블로그 주소</div>
-          </div>
-          <div className="flex-none w-1/12 flex"></div>
         </div>
-      );
+      </div>
+      <div className="flex-1 w-full">
+        <div className="text-lg text-left">{member.name}</div>
+      </div>
+      <div className="flex-none w-1/12 flex"></div>
+    </div>
+  );
 }
