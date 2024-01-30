6f319d038b1a08dd872a9c27f57e62197736ca68
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-01-30 16:28:41 +0900

    add: GitHubCallbackPage 디버깅용 code 로그 추가

--- src/page/auth/GitHubCallbackPage.tsx
+++ src/page/auth/GitHubCallbackPage.tsx
@@ -1,10 +1,6 @@
-import axios from "axios";
 import queryString from "query-string";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { PATH } from "../../constants/Path";
-import { API_PATH } from "../../constants/ApiPath";
-import { StoredMemberInfo } from "./AuthType";
 
 export default function GitHubCallbackPage() {
 
@@ -16,26 +12,28 @@ export default function GitHubCallbackPage() {
       code: code,
     };
 
-    axios
-      .post(import.meta.env.VITE_BASE_URL + API_PATH.AUTH.LOGIN, 
-      body, 
-      {
-        withCredentials: true
-      })
-      .then(function (response) {
-        if (response.status == 200) {
-            const responseData: StoredMemberInfo = response.data.data;
-            localStorage.setItem('MEMBER_INFO', JSON.stringify(responseData));
+    console.log("code : " + code)
 
-            navigate(PATH.MAIN);
-        } else {
-            throw new Error('Authentication failed');
-        }
-      })
-      .catch(function (error) {
-        console.log('error: {}', error)
-        navigate(PATH.AUTH.LOGIN);
-      });
+    // axios
+    //   .post(import.meta.env.VITE_BASE_URL + API_PATH.AUTH.LOGIN, 
+    //   body, 
+    //   {
+    //     withCredentials: true
+    //   })
+    //   .then(function (response) {
+    //     if (response.status == 200) {
+    //         const responseData: StoredMemberInfo = response.data.data;
+    //         localStorage.setItem('MEMBER_INFO', JSON.stringify(responseData));
+
+    //         navigate(PATH.MAIN);
+    //     } else {
+    //         throw new Error('Authentication failed');
+    //     }
+    //   })
+    //   .catch(function (error) {
+    //     console.log('error: {}', error)
+    //     navigate(PATH.AUTH.LOGIN);
+    //   });
 
   }, [navigate]);
 
