fc7616e42b1d8a5f978be7a3a2f79443ddbb70d4
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-01 21:28:24 +0900

    style: 폴더별 알림설정 모달에 alert 아이콘 적용

--- src/component/content/setting-folder/webhook-modal/WebHookBox.tsx
+++ src/component/content/setting-folder/webhook-modal/WebHookBox.tsx
@@ -12,7 +12,7 @@ export default function WebHookBox({ alert }: Props) {
   return (
     <div className="h-12 flex items-center hover:bg-success rounded-lg">
       <div className="flex flex-none justify-center w-1/12 mx-3">
-        <Icon name="folder" size="L" />
+        <Icon name="alert" size="L" />
       </div>
       <div className="flex-1 w-full overflow-hidden whitespace-nowrap">
         <div className="text-lg text-left">{alert.webhookUrl}</div>
