4b367cd77985f23fa9222c0243d37660c3b68aac
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-19 23:42:47 +0900

    style: post modal css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
+import "./PostContent.css";
 import LoginPage from './page/auth/LoginPage';
 import GitHubCallbackPage from './page/auth/GitHubCallbackPage';
 import { PATH } from './constants/Path';
